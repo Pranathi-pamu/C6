3e3b855c226652fdcd2773697f875bebaa048477
Pranathi-pamu <chinnupamu11@gmail.com>
2024-10-14 15:27:26 +0530

    Add files via upload

---
+++ roots of q.e.c
@@ -0,0 +1,44 @@
+#include <math.h>
+
+int main() 
+{
+     int a,b,c,d;
+     printf("enter the  a,b,c value ");
+     scanf("%d %d %d",&a,&b,&c);
+     d=b*b-4*a*c;
+     if(d>0)
+     printf("roots are real");
+     else if(d==0)
+     printf("roots are equal");
+     else 
+     printf("roots are imaginary");
+     
+     
+     
+     
+     
+    
+  
+ 
+       
+         
+       
+         
+       
+       
+     
+     
+      
+      
+      
+      
+      
+     
+     
+      
+ }    
+ 
+     
+
+
+    
